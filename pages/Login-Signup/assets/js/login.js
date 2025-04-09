f01c908580e8c669b5a99286e1e54b2a18acedf4
Daniel <Daniklj72@gmail.com>
2025-04-09 14:29:39 -0300

    new-Agendamentos+color-LOGIN/SIGNUP

--- pages/Login-Signup/assets/js/login.js
+++ pages/Login-Signup/assets/js/login.js
@@ -1,5 +1,5 @@
 const btnCadastro = document.querySelector("#btn-cadastro")
 btnCadastro.addEventListener("click", function() {
-    window.location.href="/cadastro.html";
+    window.location.href="cadastro.html";
     }
 )
